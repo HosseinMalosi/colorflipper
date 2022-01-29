a17d88153dbfe8dda0303852db7dd905e8ed6564
hossein <hossmalos@gmail.com>
2022-01-29 13:35:44 +0330

    its finishid

--- javascript.js
+++ javascript.js
@@ -3,15 +3,24 @@ const BtnReset = document.querySelector(".reset");
 const body = document.querySelector("body");
 const ColorText = document.querySelector("#Color");
 
-const colors = [
-  { name: "Red", hex: "#FF0000" },
-  { name: "Blue", hex: "#0000FF" },
-  { name: "White", hex: "#FFFFFF" },
-  { name: "Yellow", hex: "#FFFF00" },
-  { name: "Green", hex: "#00FF00" },
-];
+const hexSource = [1,2,3,4,5,7,8,9,0,"a","b","c","d","d","f"]
+
+const makingHex = function(){
+let hexCode = "#";
+for (let i = 0; i < 6; i++) {
+ hexCode += hexSource[Math.trunc(Math.random()*hexSource.length)];
+}
+return hexCode;
+}
 
 BtnChange.addEventListener("click", function () {
-  const num = Math.trunc(Math.random() * 5);
-  body.style.backgroundColor = `${colors[num].hex}`;
+body.style.backgroundColor=`${makingHex()}`
+ColorText.textContent=`${makingHex()}`
 });
+
+BtnReset.addEventListener("click", function () {
+body.style.backgroundColor="#F1F5F8";
+ColorText.textContent=`#F1F5F8`
+});
+
+
